c428910fb7a648908278942b8a5d0ae1f89d6c24
SergioUbuntu <ingensiscomescom@outlook.com>
2018-04-04 16:19:40 -0500

    Se agrego el shebang
    Se modifico "hola.py" para que tenga un shebang
    el cual busca en una direccion especifica al
    interprete de python
    Se modifico "archivo1.py" para que tenga shebang
    el cual busca dinamicamente la direccion del
    interprete de python

--- archivo1.py
+++ archivo1.py
@@ -1,2 +1,13 @@
+#! /usr/bin/env python
+#Se utiliza el programa env(de environment, entorno)
+#para preguntar al sistema por la ruta al interprete
+#de Python de esta forma nuestros usuarios no
+#tendran ningun problema si se diera el caso de que
+#el programa no estuviera instalado en dicha ruta 
 print ("Hola mundo :)")
 print "Hola mundo :v"
+print "Este programa ejecuta \"env\" y busca el interprete de Python"
+
+#Esta linea es para que el scrip no se cierre inmediatamente 
+#Ingrese "Enter" para salir 
+raw_input()
--- hola.py
+++ hola.py
@@ -1,4 +1,8 @@
-#! /usr/bin/python
+#! /usr/bin/python 
+#esta linea es conocida como shebang, hashbang o sharpbang
+#El par de caracteres "#!" indica al sistema operativo que dicho 
+#script se debe de ejecutar utilizando el interprete
+#especificado a continuacion 
 print "Hola Mundo :v"
 print "Hola en python desde ubuntu ;)"
 raw_input()
